ccb5941888ca306e91c2bc44640b373e72e6421a
Enoki <enokiun@gmail.com>
2022-09-05 21:37:25 +0300

    Revert "chore: shut the fuck up about tests"
    This reverts commit f3a8144cf2684daadb7f5611c8527ee9870eaa48.

--- src/tests.rs
+++ src/tests.rs
@@ -31,6 +31,20 @@ mod tests {
         })
         .unwrap();
 
+        let brokers = env::var("BROKERS").unwrap_or_else(|_| "localhost:9092".to_string());
+        let topic = env::var("TOPIC").unwrap_or_else(|_| "oprish".to_string());
+
+        let consumer: StreamConsumer = ClientConfig::new()
+            .set("group.id", "oprish_test")
+            .set("bootstrap.servers", &brokers)
+            .set("enable.partition.eof", "false")
+            .set("session.timeout.ms", "6000")
+            .set("auto.offset.reset", "earliest")
+            .create()
+            .unwrap();
+
+        consumer.subscribe(&[&topic]).unwrap();
+
         let response = client
             .post("/messages/")
             .body(message.clone())
@@ -39,5 +53,21 @@ mod tests {
 
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.into_string().await.unwrap(), message);
+
+        assert_eq!(
+            String::from_utf8(
+                consumer
+                    .stream()
+                    .next()
+                    .await
+                    .unwrap()
+                    .unwrap()
+                    .payload()
+                    .unwrap()
+                    .to_vec()
+            )
+            .unwrap(),
+            message
+        );
     }
 }
