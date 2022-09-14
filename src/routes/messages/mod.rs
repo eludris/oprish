01da14c3b0065c4d70de0cfdfce6291a9dd084cb
Enoki <enokiun@gmail.com>
2022-09-14 14:22:00 +0300

    feat: Added support for CloudFare ips

--- src/routes/messages/mod.rs
+++ src/routes/messages/mod.rs
@@ -1,5 +1,3 @@
-use std::net::IpAddr;
-use std::str::FromStr;
 use std::time::Duration;
 
 use crate::producer::Producer;
@@ -11,22 +9,17 @@ use rocket::{Route, State};
 use rocket_db_pools::Connection;
 use serde_valid::Validate;
 use todel::models::Message;
-use todel::oprish::{MessageCreateResponse, RatelimitedRoutResponse};
+use todel::oprish::{ClientIP, MessageCreateResponse, RatelimitedRoutResponse};
 
 #[post("/", data = "<message>")]
 pub async fn index(
     message: Json<Message>,
     producer: &State<Producer>,
-    address: Option<IpAddr>,
+    address: ClientIP,
     cache: Connection<Cache>,
 ) -> RatelimitedRoutResponse<(Status, Json<MessageCreateResponse>)> {
-    let mut ratelimiter = Ratelimiter::new(
-        cache,
-        "message_send",
-        address.unwrap_or_else(|| IpAddr::from_str("127.0.0.1").unwrap()),
-        Duration::from_secs(5),
-        10,
-    );
+    let mut ratelimiter =
+        Ratelimiter::new(cache, "message_send", address, Duration::from_secs(5), 10);
     ratelimiter.process_ratelimit().await?;
     let message = message.into_inner();
     if let Err(err) = message.validate() {
