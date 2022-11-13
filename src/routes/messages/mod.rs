2f6a8b32960cbb6f3bc773309a4789a56f38f000
Enoki <enokiun@gmail.com>
2022-11-13 23:53:22 +0300

    refactor: improve how the message_create route works

--- src/routes/messages/mod.rs
+++ src/routes/messages/mod.rs
@@ -1,44 +1,52 @@
 use crate::ratelimit::Ratelimiter;
 use crate::Cache;
 use deadpool_redis::redis::AsyncCommands;
-use rocket::http::Status;
 use rocket::serde::json::Json;
-use rocket::Route;
+use rocket::{Route, State};
 use rocket_db_pools::Connection;
-use serde::Serialize;
-use serde_valid::validation::Errors;
-use serde_valid::Validate;
-use std::time::Duration;
 use todel::models::Message;
-use todel::oprish::{ClientIP, RatelimitedRoutResponse};
-
-#[derive(Debug, Serialize)]
-#[serde(untagged)]
-pub enum MessageCreateResponse {
-    Success(Message),
-    ValidationError(Errors),
-}
+use todel::oprish::{
+    ClientIP, ErrorResponse, ErrorResponseData, RatelimitedRoutResponse, Response,
+};
+use todel::Conf;
 
 #[post("/", data = "<message>")]
 pub async fn index(
     message: Json<Message>,
     address: ClientIP,
     mut cache: Connection<Cache>,
-) -> RatelimitedRoutResponse<(Status, Json<MessageCreateResponse>)> {
-    let mut ratelimiter = Ratelimiter::new("message_send", address, Duration::from_secs(5), 10);
+    conf: &State<Conf>,
+) -> RatelimitedRoutResponse<Json<Response<Message>>> {
+    let mut ratelimiter = Ratelimiter::new("message_create", address, conf.inner());
     ratelimiter.process_ratelimit(&mut cache).await?;
     let message = message.into_inner();
-    if let Err(err) = message.validate() {
-        Ok(ratelimiter.wrap_response((
-            Status::BadRequest,
-            Json(MessageCreateResponse::ValidationError(err)),
-        )))
+    if message.author.len() < 2 || message.author.len() > 32 {
+        Ok(
+            ratelimiter.wrap_response(Json(Response::Failure(ErrorResponse::new(
+                ErrorResponseData::ValidationError {
+                    invalid_key: "author".to_string(),
+                    info: "Message author has to be between 2 and 32 characters long.".to_string(),
+                },
+            )))),
+        )
+    } else if message.content.is_empty() || message.content.len() > conf.oprish.message_limit {
+        Ok(
+            ratelimiter.wrap_response(Json(Response::Failure(ErrorResponse::new(
+                ErrorResponseData::ValidationError {
+                    invalid_key: "content".to_string(),
+                    info: format!(
+                        "Message content has to be between 1 and {} characters long.",
+                        conf.oprish.message_limit
+                    ),
+                },
+            )))),
+        )
     } else {
         cache
             .publish::<&str, String, ()>("oprish-events", serde_json::to_string(&message).unwrap())
             .await
             .unwrap();
-        Ok(ratelimiter.wrap_response((Status::Ok, Json(MessageCreateResponse::Success(message)))))
+        Ok(ratelimiter.wrap_response(Json(Response::Success(message))))
     }
 }
 
