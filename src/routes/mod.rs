8ac4a02f45388ef73f4a3a7f57ce6a7fea8a8edf
Enoki <enokiun@gmail.com>
2022-11-13 23:53:43 +0300

    feat: add ratelimiting to the info route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,13 +1,30 @@
 pub mod messages;
+pub mod ratelimits;
 
 use rocket::{serde::json::Json, Route, State};
-use todel::models::Info; // poggers
+use rocket_db_pools::Connection;
+use todel::{
+    models::Info,
+    oprish::{ClientIP, RatelimitedRoutResponse},
+    Conf,
+};
+
+use crate::{ratelimit::Ratelimiter, Cache}; // poggers
 
 #[get("/")]
-pub async fn index(info: &State<Info>) -> Json<&Info> {
-    Json(info.inner())
+pub async fn index(
+    address: ClientIP,
+    mut cache: Connection<Cache>,
+    conf: &State<Conf>,
+) -> RatelimitedRoutResponse<Json<Info>> {
+    let mut ratelimiter = Ratelimiter::new("info", address, conf.inner());
+    ratelimiter.process_ratelimit(&mut cache).await?;
+    Ok(ratelimiter.wrap_response(Json(Info {
+        instance_name: conf.instance_name.clone(),
+        description: conf.description.clone(),
+    })))
 }
 
 pub fn get_routes() -> Vec<Route> {
-    routes![index]
+    routes![index, ratelimits::ratelimits]
 }
