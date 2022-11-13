8cdc3d3f64f1cb7e00cc39732db9919a9fedfb3e
Enoki <enokiun@gmail.com>
2022-11-13 23:20:32 +0300

    feat: switch to using todel::Conf

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,7 @@ mod routes;
 use rocket::{Build, Rocket};
 use rocket_db_pools::Database;
 use routes::*;
-use std::env;
-use todel::models::{Feature, Info};
+use todel::Conf;
 
 #[derive(Database)]
 #[database("redis-cache")]
@@ -20,29 +19,15 @@ pub struct Cache(deadpool_redis::Pool);
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    #[cfg(test)]
-    {
-        env::set_var("INSTANCE_NAME", "WooChat")
-    }
     dotenv::dotenv().ok();
     env_logger::try_init().ok();
 
-    let instance_name =
-        env::var("INSTANCE_NAME").expect("Can't find \"INSTANCE_NAME\" environment variable");
-
-    let features = vec![Feature {
-        id: 0,
-        name: "base".to_string(),
-    }];
-    let info = Info {
-        instance_name,
-        features,
-    };
+    let conf = Conf::new_from_env();
 
     rocket::build()
         .mount("/", get_routes())
         .mount("/messages", messages::get_routes())
-        .manage(info)
+        .manage(conf)
         .attach(Cache::init())
         .attach(cors::Cors)
 }
